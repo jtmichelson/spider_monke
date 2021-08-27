d5792f5a1b7e0a856ad5d751e72e82b59694074a
Jason Michelson <jasonmichelson@Jasons-MacBook-Pro.local>
2021-08-26 22:36:57 -0700

    Update assets, remove unused imports.

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
 # import the necessary packages
-from collections import deque
-from imutils.video import VideoStream
 import numpy as np
-import argparse
 import cv2
-import imutils
-import time
 from mss import mss
-from PIL import Image
 
-bounding_box = {'top': 100, 'left': 0, 'width': 400, 'height': 300}
+bounding_box = {'top': 100, 'left': 0, 'width': 1920, 'height': 1080}
 
-sct = mss()
+screen_shotter = mss()
 
 while True:
-    sct_img = sct.grab(bounding_box)
-    # cv2.imshow('screen', np.array(sct_img))
-    img_rgb = np.array(sct_img)
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    screenshot_raw = screen_shotter.grab(bounding_box)
+    screenshot_rgb = np.array(screenshot_raw)
+    screenshot_gray = cv2.cvtColor(screenshot_rgb, cv2.COLOR_BGR2GRAY)
 
-    template = cv2.imread('spider_right.png', 0)
-    w, h = template.shape[::-1]
+    template = cv2.imread('green_spider_down_1_64x64.png', 0)
+    width, height = template.shape[::-1]
 
-    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+    resolution = cv2.matchTemplate(screenshot_gray, template, cv2.TM_CCOEFF_NORMED)
     threshold = 0.5
-    loc = np.where(res >= threshold)
+    locations = np.where(resolution >= threshold)
 
-    for pt in zip(*loc[::-1]):
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
+    for point in zip(*locations[::-1]):
+        cv2.rectangle(screenshot_rgb, point, (point[0] + width, point[1] + height), (0, 255, 255), 2)
 
-    cv2.imshow('Detected', img_rgb)
+    cv2.imshow('Detected', screenshot_rgb)
 
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
         cv2.destroyAllWindows()
